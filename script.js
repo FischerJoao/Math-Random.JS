13df18161b920c3518d74b0af636392ff64bc726
DevJoaoFischer <joao-fischer@hotmail.com>
2022-12-09 10:27:11 -0300

    histoy of numbers added

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function gerar() {
     let numero = Math.floor(Math.random() * 99) + 1;
     let msg = document.getElementById('msg')
+    let sorteados = document.getElementById('sorteados')
     msg.innerHTML = `O número sorteado foi:`
-    result.innerHTML = `${numero}`
+    result.innerHTML = `👉${numero}👈`
+    sorteados.innerHTML += `${numero}, `
     
 }
 
 function apagar() {
-    msg.innerHTML = 'O número aparecerá aqui:'
+    msg.innerHTML = 'O número aparecerá aqui : <br> 👇  '
     result.innerHTML = ''
+    sorteados.innerHTML = 'Números já sorteados aparecem aqui: 👉'
+
 }
